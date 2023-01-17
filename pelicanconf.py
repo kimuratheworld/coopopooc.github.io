453dd65e768e9c757b294f899833343b31a231b7
coopopooc <tangofox22@outlook.com>
2023-01-17 08:49:33 -0500

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,64 @@
+AUTHOR = 'Jafet Rubi'
+SITENAME = 'Jafet Rubi'
+SITEURL = 'http://127.0.0.1:8000/'
+
+PATH = 'content'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'EN'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# # Blogroll
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# # Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Theme
+THEME = 'minimalxy'
+
+# Theme customizations
+# MINIMALXY_CUSTOM_CSS = 'static/custom.css'
+# MINIMALXY_FAVICON = 'favicon.ico'
+MINIMALXY_START_YEAR = 2022
+MINIMALXY_CURRENT_YEAR =  2023
+
+# Author
+AUTHOR_INTRO = u'Hello world! I’m John Doe.'
+AUTHOR_DESCRIPTION = u'Hello world! I’m John Doe. I like coffee, birds and Python.'
+AUTHOR_AVATAR = 'http://www.gravatar.com/avatar/abcdefghijkl?s=240'
+AUTHOR_WEB = ''
+
+# Services
+# GOOGLE_ANALYTICS = 'UA-12345678-9'
+# DISQUS_SITENAME = 'johndoe'
+
+# Social
+SOCIAL = (
+    # ('facebook', 'http://www.facebook.com/johndoe'),
+    # ('twitter', 'http://twitter.com/johndoe'),
+    ('github', 'https://github.com/johndoe'),
+    ('linkedin', 'http://www.linkedin.com/in/johndoe'),
+)
+
+# Menu
+MENUITEMS = (
+    # ('Categories', '/' + CATEGORIES_SAVE_AS),
+    # ('Archive', '/' + ARCHIVES_SAVE_AS),
+)
